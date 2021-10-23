9b84d2e8dd8859cb22a8fddcb5d6416d26c9087c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-10-23 22:00:18 +0300

    init

--- HowToUseDependencyInjectionInSwiftUI/ContentView.swift
+++ HowToUseDependencyInjectionInSwiftUI/ContentView.swift
@@ -6,11 +6,65 @@
 //
 
 import SwiftUI
+import Combine
+
+struct PostModel: Identifiable, Codable {
+    
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class ProductionDataService {
+    
+    static let instance = ProductionDataService()
+    
+    let url: URL = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+    
+    func getData() -> AnyPublisher<[PostModel], Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map({ $0.data })
+            .decode(type: [PostModel].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
+
+class DependencyInjectionViewModel: ObservableObject {
+    
+    @Published var dataArray: [PostModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        loadPosts()
+    }
+    
+    private func loadPosts() {
+        ProductionDataService.instance.getData()
+            .sink { _ in
+                
+            } receiveValue: { [weak self] returnedPosts in
+                self?.dataArray = returnedPosts
+            }
+            .store(in: &cancellables)
+
+    }
+    
+}
 
 struct ContentView: View {
+    
+    @StateObject private var vm = DependencyInjectionViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            VStack {
+                ForEach(vm.dataArray) { post in
+                    Text(post.title)
+                }
+            }
+        }
     }
 }
 
