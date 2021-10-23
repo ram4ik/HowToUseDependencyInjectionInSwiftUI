ae14a1c28ab42f9baa2645d96ef5d0fa5b3fd3e5
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-10-23 21:40:49 +0300

    Initial Commit

---
+++ HowToUseDependencyInjectionInSwiftUI/HowToUseDependencyInjectionInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseDependencyInjectionInSwiftUIApp.swift
+//  HowToUseDependencyInjectionInSwiftUI
+//
+//  Created by Ramill Ibragimov on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseDependencyInjectionInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
